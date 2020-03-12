ae4bec10217b1d69e74120a1230ff3dd17cfe9cf
JZhanSZ <53909543+JZhanSZ@users.noreply.github.com>
2020-03-12 22:17:01 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+int main()
+{
+  vector<int> input = {3 , 12, 15, 7 , 20, 13, 33, 333, -3,21,23};
+  
+
+  
+  for (int i = 0; i < series.size(); i++)
+{
+	cout << series.at(i) << " ";
+}
+  
+
+	return 0;
+}

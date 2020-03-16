8acfad8b5ad881b5487742d8e0714e53fd7127b8
JZhanSZ <53909543+JZhanSZ@users.noreply.github.com>
2020-03-17 04:19:07 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include "bubble_sort.h"
 
 int main()
 {
-  vector<int> input = {3 , 12, 15, 7 , 20, 13, 33, 333, -3,21,23};
+  std::vector<int> input = {3 , 12, 15, 7 , 20, 13, 33, 333, -3,21,23};
   
+    BubbleSort::sort(input);
 
-  
-  for (int i = 0; i < series.size(); i++)
+//displace
+  for (int i = 0; i < input.size(); i++)
 {
-	cout << series.at(i) << " ";
+	std::cout << input.at(i) << " ";
 }
-  
 
 	return 0;
 }
